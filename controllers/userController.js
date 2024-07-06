03800397fd321d65eaa6964aa79e773b1bdabfba
CesarCadena5 <cesarojitos555@hotmail.com>
2024-07-06 15:10:42 -0500

    cambios en user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,8 +16,9 @@ export class UserController {
             const token = await generateToken(userSave._id);
 
             res.cookie('access_token', token, {
-                httpOnly: false,
-                maxAge: 1000 * 60 * 60
+                httpOnly: true,
+                maxAge: 1000 * 60 * 60,
+                domain: 'https://tienda-cesar.netlify.app'
             }).json({
                 icon: 'success',
                 msg: 'Usuario creado con éxito.',
